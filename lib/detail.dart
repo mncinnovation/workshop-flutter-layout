934eb2bd838f62fcbec096b698c2b2d5c530b56d
Muhammad Khair <muhammad.rahmatul@mncgroup.com>
2021-03-18 13:35:18 +0700

    initial commit

---
+++ lib/detail.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Detail extends StatefulWidget {
+  Detail({Key key}) : super(key: key);
+
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Page detail'),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout_app/detail.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(90),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 3.0,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/images/logo_celebritiesdotid.png',
+                scale: 20 / 9,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Detail()));
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Headline',
+                    style:
+                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Divider(
+                height: 3,
+                color: Colors.red[600],
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+                child: Image.asset('assets/images/foto_bts.png'),
+              ),
+              Text(
+                'Entertaiment',
+                textAlign: TextAlign.start,
+                style: TextStyle(color: Colors.red[600]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  'Siap-Siap, BTS Dikabarkan Akan Rilis Album BE Versi Terbaru',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
